6e092845aa13bb833b3e6586e51a004470527fbf
bazooka07 <jpourrez@gmail.com>
2019-11-26 18:03:22 +0100

    Construction auto de l'image avec run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,31 @@
 #!/bin/sh
 
+IMAGE="pluxml:alpine"
 TARGET="site"
 
+if [ -z $(command -v docker) ]; then
+	echo << EOT
+Docker is required
+Look at :
+https://docs.docker.com/install/linux/docker-ce/ubuntu/
+
+EOT
+	exit
+fi
+
+if [ -z  $(docker image ls $IMAGE --format '{{.ID}}') ]; then
+	echo "\e[91m $IMAGE is building. Please wait for it \e[m"
+       	docker build . -t $IMAGE
+	echo "\e[32mDone\e[m\n"
+fi
+
+OTHER="docker run --rm -tiv $(pwd)/$TARGET:/web $IMAGE /bin/sh"
+echo "\nExecute the following command for interaction with the image :\n\e[36m$OTHER\e[m\n"
+
+echo "\e[33m --------- Image Docker : $IMAGE ---------\e[m"
+docker image ls $IMAGE --format 'table{{.CreatedSince}}\t{{.Size}}\t{{.ID}}'
+
+
 [ -d $TARGET ] || mkdir $TARGET
-docker run --rm -iv $(pwd)/$TARGET:/web pluxml:alpine
-# docker run --rm -tiv $(pwd)/$TARGET:/web pluxml:alpine /bin/sh
+docker run --rm -iv $(pwd)/$TARGET:/web $IMAGE
+
